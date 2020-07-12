11ffc7f04b4b793d48b42f9f70ffec0a5eaca566
MJ <audwns232@naver.com>
2020-07-12 18:49:53 +0900

    Finish roughly - header - html, css, js

--- menu-icon.js
+++ menu-icon.js
@@ -1,6 +1,9 @@
 const icon = document.querySelector(".js-icon");
+const menu = document.querySelector(".menu-clicked");
 
-function handleClick(event) {}
+function handleClick(event) {
+  menu.classList.toggle("showing");
+}
 
 function init() {
   icon.addEventListener("click", handleClick);
